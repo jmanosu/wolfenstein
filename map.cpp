6e18067cbb4f7be77a7e61b2bb5971716ec9e16e
Jared Tence <jmanosu@gmail.com>
2018-12-10 17:27:55 -0800

    template-made

--- map.cpp
+++ map.cpp
@@ -65,9 +65,9 @@ void Map::defaultMap(int width, int length){
     floors[0][i] = new Wall[width];
     for(int j = 0; j < width; j++){
       //initalizes the wall object to have a cascading color
-      if(i == 1 && j == 2 || i == 1 && j == 1){
+      if(i == 1 && j == 2){
           temp.init(250, j*i, j*i, true);
-      }else if(i == 2 && j == 2){
+      }else if(i == 1 && j == 1){
           temp.init(250, j*i, j*i, true);
       }else if(i == 2 && j == 1){
           temp.init(250, j*i, j*i, true);
